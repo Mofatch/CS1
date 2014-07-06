6810d234667698ff80d44df71f3b8dc1f42ed671
Travis <travis.bashor@gmail.com>
2014-07-06 10:37:32 -0400

    modulus, power

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "PriorityQueue.c"
 #include "Objects.c"
 #include "DArray.c"
@@ -26,7 +27,7 @@ int postfix(int numArguments, char **argList) {
   PQMode myMode = PQMODE_STACK;
   PQueue *myStack = allocPQueue(sizeof(int), myMode);
   int i;
-  int result, higher, lower;
+  int result, higher, lower, element;
 
   // go through the list, collecting arguments
   for(i = 1; i < numArguments; ++i) {
@@ -61,6 +62,23 @@ int postfix(int numArguments, char **argList) {
         lower = *(int *)top(myStack);
         pop(myStack);
         push(myStack, allocIntWith(lower / higher));
+        printf("Top: %d\n", *(int *)top(myStack));
+        break;
+      case '%':
+        higher = *(int *)top(myStack);
+        pop(myStack);
+        lower = *(int *)top(myStack);
+        pop(myStack);
+        push(myStack, allocIntWith(lower % higher));
+        printf("Top: %d\n", *(int *)top(myStack));
+        break;
+      case '^':
+        higher = *(int *)top(myStack);
+        pop(myStack);
+        lower = *(int *)top(myStack);
+        pop(myStack);
+        element = (higher < 1) ? 0 : pow(lower, higher);
+        push(myStack, allocIntWith(element));
         printf("Top: %d\n", *(int *)top(myStack));
         break;
       default:
