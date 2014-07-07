32d71e7401b2e3f47359ae4f70181d3a81942cfc
Travis <travis.bashor@gmail.com>
2014-07-07 18:38:17 -0400

    updated actionProcess() to include msgs

--- Lab3/rpg.c
+++ Lab3/rpg.c
@@ -1,36 +1,45 @@
 #include "rpg.h"
 
 bool actionProcess(GameSprite* gs, ArrayList* players, ArrayList* enemies) {
-  int num = rand() % 101;
+  // target of action
   GameSprite *target;
+  int roll;
 
-  if(num < gs->accuracy) {
+  if(gs->type == ENEMY) {
+    target = (GameSprite *)getDArray(players, (rand() % players->length));
+    printf("%d attacks %d!", gs->id, target->id);
+  }
+  else if(gs->type == FIGHTER) {
+    target = (GameSprite *)getDArray(enemies, (rand() % enemies->length));
+    printf("%d attacks %d!", gs->id, target->id);
+  }
+  else if(gs->type == HEALER) {
+    target = (GameSprite *)getDArray(players, (rand() % players->length));
+    printf("%d attempts to heal %d!", gs->id, target->id);
+  }
+  // random number in [0,100]
+  roll = rand() % 101;
+
+  if(roll < gs->accuracy) {
     switch(gs->type) {
       case ENEMY:
         // get a random ally and reduce its hp
-        if(players->length > 0) {
-          target = (GameSprite *)getDArray(players, (rand() % players->length));
-          target->hp -= gs->strength;
-        }
+        target->hp -= gs->strength;
         break;
       case FIGHTER:
         // get a random enemy and reduce its hp
-        if(enemies->length > 0) {
-          target = (GameSprite *)getDArray(enemies, (rand() % enemies->length));
-          target->hp -= gs->strength;
-        }
+        target->hp -= gs->strength;
         break;
       case HEALER:
         // heal random ally
-        if(players->length > 0) {
-          target = (GameSprite *)getDArray(players, (rand() % players->length));
-          target->hp += gs->strength;
-        }
+        target->hp += gs->strength;
         break;
     }
     // hit
+    printf("Hit!\n");
     return true;
   }
-  // no hit
+  // miss
+  printf("Miss!\n");
   return false;
 }
