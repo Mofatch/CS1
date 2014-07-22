e1534e355a662e7c446ed62a3766ac0390593cb1
Travis Bashor <travis.bashor@gmail.com>
2014-07-22 13:16:30 -0400

    add pdf and main to lab 5 folder
    main template and pdf

---
+++ Lab5/main.c
@@ -0,0 +1,34 @@
+/* Authors: Travis Bashor/Trevor Minnix
+  Date: 7/31/14
+  Course Info: COP 3502, Summer C 2014 
+  Description: The purpose of this program is to demonstrate
+  the use of Hash Tables with two classes: Course and Student.
+*/ 
+
+// includes
+#include <iostream>
+
+// prototypes
+void createStudent(map<string, Student>);
+Course createCourse();
+void addCourse(Student, Course);
+void printStudents();
+
+int main() {
+  // CODE
+  return 0;
+}
+
+// main functions
+void createStudent(map<string str, Student stud>) {
+  // CODE
+}
+Course createCourse() {
+  // CODE
+}
+void addCourse(Student stud, Course crs) {
+  // CODE
+}
+void printStudents() {
+  // CODE
+}
